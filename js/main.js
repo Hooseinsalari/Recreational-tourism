da8d4945dd56c524d2b62abe4bed7e1edcef4337
Hooseinsalari <hossein.salari002@gmail.com>
2022-06-04 15:15:53 +0430

    create container

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ const showMenu = document.querySelector('.nav__toggle');
 const closeMenu = document.querySelector('.nav__close');
 const navMenu = document.querySelector('.nav__menu');
 const navLink = document.querySelectorAll('.nav__link');
+const navbar = document.querySelector('.nav');
 
+// hamburger menu
 showMenu.addEventListener('click', () => {
     navMenu.classList.add('active__menu')
 })
@@ -16,3 +18,13 @@ navLink.forEach((a) => {
         navMenu.classList.remove('active__menu')
     })
 })
+
+// change nav color while scroll
+
+window.onscroll = function() {
+    if(document.body.scrollTop >= 80 || document.documentElement.scrollTop >= 80) {
+        navbar.classList.add('scroll');
+    } else {
+        navbar.classList.remove('scroll');
+    }
+}
